f197604770dfe65105416d4de17966c3f39907d7
cszczepaniak <cszczepaniak@gmail.com>
2019-11-04 21:04:49 -0600

    rename point to point2

--- SnailTrail/point/point.go
+++ SnailTrail/point/point.go
@@ -4,14 +4,14 @@ import (
 	"github.com/cszczepaniak/fivethirtyeight-riddler/SnailTrail/utils"
 )
 
-// Point represents a cartesian point
-type Point struct {
+// Point2 represents a cartesian point
+type Point2 struct {
 	X float64
 	Y float64
 }
 
 // DistanceTo calculates the distance between this point and another
-func (p Point) DistanceTo(pt Point) float64 {
+func (p Point2) DistanceTo(pt Point2) float64 {
 	coords := make([]float64, 2)
 	coords[0] = p.X - pt.X
 	coords[1] = p.Y - pt.Y
--- SnailTrail/point/point_test.go
+++ SnailTrail/point/point_test.go
@@ -10,26 +10,26 @@ import (
 func TestPoint_DistanceTo(t *testing.T) {
 	tests := []struct {
 		name   string
-		pt     Point
+		pt     Point2
 		expect float64
 	}{
 		{
 			name:   "test easy distance",
-			pt:     Point{X: 3, Y: 4},
+			pt:     Point2{X: 3, Y: 4},
 			expect: 5,
 		},
 		{
 			name:   "test negative point",
-			pt:     Point{X: 3, Y: -4},
+			pt:     Point2{X: 3, Y: -4},
 			expect: 5,
 		},
 		{
 			name:   "test irrational",
-			pt:     Point{X: 1, Y: 1},
+			pt:     Point2{X: 1, Y: 1},
 			expect: math.Sqrt(2),
 		},
 	}
-	refPt := Point{X: 0, Y: 0}
+	refPt := Point2{X: 0, Y: 0}
 	for _, tc := range tests {
 		dist := refPt.DistanceTo(tc.pt)
 		assert.InEpsilon(t, tc.expect, dist, 1e-8, tc.name)
