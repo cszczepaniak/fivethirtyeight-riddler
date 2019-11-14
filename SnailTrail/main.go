a9f8c937d3f6e071c0cc806a5fa78513dd4b7f05
Connor Szczepaniak <cszczepaniak@gmail.com>
2019-11-14 10:36:25 -0600

    add timing and more output info plus ability to set max steps

--- SnailTrail/main.go
+++ SnailTrail/main.go
@@ -4,17 +4,26 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/cszczepaniak/fivethirtyeight-riddler/SnailTrail/setup"
 	"github.com/cszczepaniak/fivethirtyeight-riddler/SnailTrail/snail"
 )
 
 func main() {
+	start := time.Now()
+	timeFunc := func() {
+		elapsed := time.Since(start)
+		fmt.Printf("Time elapsed: %f\n", elapsed.Seconds())
+	}
+	defer timeFunc()
+
 	var stepSize float64
+	var maxSteps int
 	flag.Float64Var(&stepSize, `stepsize`, 0.01, `The step size the snails will take`)
+	flag.IntVar(&maxSteps, `maxsteps`, 1000000, `The step size the snails will take`)
 	flag.Parse()
 
-	const maxSteps = 1000000
 	var dist float64 = 0
 	step := 0
 
@@ -31,4 +40,5 @@ func main() {
 		}
 	}
 	fmt.Printf("Total distance traveled: %0.2f meters\n", dist)
+	fmt.Printf("Total steps taken: %d\n", step)
 }
