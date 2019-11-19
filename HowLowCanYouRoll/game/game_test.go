473052d1922e3802dda798c2ab25ed26a0577832
cszczepaniak <cszczepaniak@gmail.com>
2019-11-18 21:38:11 -0600

    add more test cases

--- HowLowCanYouRoll/game/game_test.go
+++ HowLowCanYouRoll/game/game_test.go
@@ -9,15 +9,23 @@ import (
 func Test_updateScore(t *testing.T) {
 	tests := []struct {
 		name   string
-		score  int64
 		rolls  []int64
 		expect int64
 	}{
 		{
-			name:   `easy test`,
-			score:  9,
+			name:   `simple test`,
 			rolls:  []int64{9, 1},
 			expect: 91,
+		},
+		{
+			name:   `test repeated digits`,
+			rolls:  []int64{9, 9, 7},
+			expect: 997,
+		},
+		{
+			name:   `test higher digits`,
+			rolls:  []int64{7, 9, 6},
+			expect: 76,
 		},
 	}
 	for _, tc := range tests {
