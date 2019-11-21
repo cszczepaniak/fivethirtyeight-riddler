8a503c3e19a0c753476433d8d140c37e89597227
cszczepaniak <cszczepaniak@gmail.com>
2019-11-20 20:06:38 -0600

    finish Play function

--- HowLowCanYouRoll/game/game.go
+++ HowLowCanYouRoll/game/game.go
@@ -4,15 +4,20 @@ import (
 	"math/rand"
 )
 
-func Play() {
+func Play() string {
 	var score string = `0.`
 	for {
 		roll := rand.Intn(10)
-		if roll == 0 {
-			break
-		}
 		updateScore(&score, roll)
+		if lastDigit(score) == '0' {
+			return score
+		}
 	}
+}
+
+func lastDigit(s string) rune {
+	runes := []rune(s)
+	return runes[len(runes)-1]
 }
 
 func updateScore(score *string, roll int) {
--- HowLowCanYouRoll/main.go
+++ HowLowCanYouRoll/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/cszczepaniak/fivethirtyeight-riddler/HowLowCanYouRoll/game"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(game.Play())
+	}
 }
