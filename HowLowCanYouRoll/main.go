d6828b2d8aa744b7c56bcbae5b19cb948a1420fa
cszczepaniak <cszczepaniak@gmail.com>
2019-11-22 07:51:49 -0600

    make main calculate the average

--- HowLowCanYouRoll/main.go
+++ HowLowCanYouRoll/main.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math/big"
+	"time"
 
 	"github.com/cszczepaniak/fivethirtyeight-riddler/HowLowCanYouRoll/game"
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(game.Play())
+	defer printTime(time.Now())
+
+	nGames := 100000000
+	nWorkers := 100
+	sum := big.NewFloat(0)
+	results := make(chan *big.Float)
+	for i := 0; i < nWorkers; i++ {
+		go worker(nGames/nWorkers, results)
 	}
+	for i := 0; i < nGames; i++ {
+		sum.Add(sum, <-results)
+	}
+	avg := new(big.Float).Quo(sum, big.NewFloat(float64(nGames)))
+	fmt.Println(avg)
+}
+
+func worker(nGames int, results chan<- *big.Float) {
+	for i := 0; i < nGames; i++ {
+		results <- game.Play()
+	}
+}
+
+func printTime(start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Printf("execution took %0.5f sec\n", elapsed.Seconds())
 }
