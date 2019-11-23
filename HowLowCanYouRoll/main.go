3c3517da3b71f16dc9bf85eecda22d49cd133381
cszczepaniak <cszczepaniak@gmail.com>
2019-11-23 14:32:53 -0600

    fix main to use new float func

--- HowLowCanYouRoll/main.go
+++ HowLowCanYouRoll/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/cszczepaniak/fivethirtyeight-riddler/HowLowCanYouRoll/game"
 )
 
 func main() {
-	game.Play()
-	return
+	defer printTime(time.Now())
 
-	// defer printTime(time.Now())
-
-	// nGames := 100000000
-	// nWorkers := 100
-	// sum := big.NewFloat(0)
-	// results := make(chan *big.Float)
-	// for i := 0; i < nWorkers; i++ {
-	// 	go worker(nGames/nWorkers, results)
-	// }
-	// for i := 0; i < nGames; i++ {
-	// 	sum.Add(sum, <-results)
-	// }
-	// avg := new(big.Float).Quo(sum, big.NewFloat(float64(nGames)))
-	// fmt.Println(avg)
+	nGames := 100000000
+	nWorkers := 100
+	var sum float64 = 0
+	results := make(chan float64)
+	for i := 0; i < nWorkers; i++ {
+		go worker(nGames/nWorkers, results)
+	}
+	for i := 0; i < nGames; i++ {
+		sum += <-results
+	}
+	avg := sum / float64(nGames) //new(big.Float).Quo(sum, big.NewFloat(float64(nGames)))
+	fmt.Println(avg)
 }
 
-// func worker(nGames int, results chan<- *big.Float) {
-// 	for i := 0; i < nGames; i++ {
-// 		results <- game.Play()
-// 	}
-// }
+func worker(nGames int, results chan<- float64) {
+	for i := 0; i < nGames; i++ {
+		results <- game.Play()
+	}
+}
 
-// func printTime(start time.Time) {
-// 	elapsed := time.Since(start)
-// 	fmt.Printf("execution took %0.5f sec\n", elapsed.Seconds())
-// }
+func printTime(start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Printf("execution took %0.5f sec\n", elapsed.Seconds())
+}
