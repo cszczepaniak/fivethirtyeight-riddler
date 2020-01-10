cd5be9c7a146808d6437069cba3cf4155dd81c95
cszczepaniak <cszczepaniak@danfoss.com>
2020-01-10 15:36:19 -0600

    start build out what we'll need to solve this

--- SpellingBee/main.go
+++ SpellingBee/main.go
@@ -1,12 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 )
+
+type letterSet map[rune]struct{}
+
+func newLetterSet(s string) letterSet {
+	runes := []rune(s)
+	ls := make(map[rune]struct{})
+	for _, r := range runes {
+		if _, ok := ls[r]; !ok {
+			ls[r] = struct{}{}
+		}
+	}
+	return ls
+}
+
+type board struct {
+	middle rune
+	others letterSet
+}
+
+func newBoard(middle rune, others []rune) (board, error) {
+	if len(others) != 6 {
+		return board{}, errors.New(`a board must have six outer letters`)
+	}
+	letters := make(map[rune]struct{}, len(others))
+	for _, r := range others {
+		if _, ok := letters[r]; !ok {
+			letters[r] = struct{}{}
+		} else {
+			return board{}, errors.New(`a board cannot contain duplicate letters`)
+		}
+	}
+	if _, ok := letters[middle]; ok {
+		return board{}, errors.New(`a board cannot contain duplicate letters`)
+	}
+	return board{
+		middle: middle,
+		others: letters,
+	}, nil
+}
+
+type word struct {
+	str     string
+	letters letterSet
+}
+
+func newWord(w string) word {
+	return word{
+		str:     w,
+		letters: newLetterSet(w),
+	}
+}
 
 func main() {
 	if !fileExists(`words.txt`) {
@@ -16,45 +67,57 @@ func main() {
 		}
 	}
 
-	if !fileExists(`filtered.txt`) {
-		err := filterWords()
-		if err != nil {
-			panic(err)
-		}
+	words, err := filterWords()
+	if err != nil {
+		panic(err)
 	}
 }
 
-func filterWords() error {
-	in, err := os.Open(`words.txt`)
-	if err != nil {
-		return err
+func buildLetterSet(word string) letterSet {
+	runes := []rune(word)
+	letterSet := make(map[rune]struct{})
+	for _, r := range runes {
+		if _, ok := letterSet[r]; !ok {
+			letterSet[r] = struct{}{}
+		}
 	}
-	defer in.Close()
+	return letterSet
+}
 
-	bytes, err := ioutil.ReadAll(in)
+func filterWords() ([]string, error) {
+	words, err := readWordsFromFile(`words.txt`)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	txt := string(bytes)
-	words := strings.Split(txt, "\n")
-
-	out, err := os.Create(`filtered.txt`)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
+	res := make([]string, 0, len(words))
 	for _, w := range words {
 		if len([]rune(w)) < 4 || strings.Contains(w, `s`) {
 			continue
 		}
-		_, err := fmt.Fprintf(out, "%s\n", w)
+		res = append(res, w)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
-	return nil
+
+	return res, nil
+}
+
+func readWordsFromFile(file string) ([]string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	txt := string(bytes)
+	return strings.Split(txt, "\n"), nil
 }
 
 func downloadWords() error {
