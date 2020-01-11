917d4d8ff72c612175bff2675d7adf3deb3c234f
cszczepaniak <cszczepaniak@gmail.com>
2020-01-11 11:38:41 -0600

    start to build the words

--- Gematria/main.go
+++ Gematria/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
 
 func main() {
 
+}
+
+func numberAsWord(n int) (string, error) {
+	if n < 1 {
+		return ``, errors.New(`number must be 1 or greater`)
+	}
+	str := firstTwoDigits(n)
+	return str, nil
+}
+
+func firstTwoDigits(n int) string {
+	n %= 100
+	switch {
+	case n < 10:
+		return onesMap[n]
+	case n < 20:
+		return teensMap[n]
+	}
+	return tensMap[n/10] + onesMap[n%10]
 }
 
 func wordValue(w string) int {
--- Gematria/main_test.go
+++ Gematria/main_test.go
@@ -6,6 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFirstTwoDigits(t *testing.T) {
+	tests := []struct {
+		n   int
+		exp string
+	}{{
+		n:   123,
+		exp: `twentythree`,
+	}, {
+		n:   13,
+		exp: `thirteen`,
+	}, {
+		n:   99,
+		exp: `ninetynine`,
+	}, {
+		n:   1,
+		exp: `one`,
+	}}
+	for _, tc := range tests {
+		w := firstTwoDigits(tc.n)
+		assert.Equal(t, tc.exp, w)
+	}
+}
+
 func TestWordValue(t *testing.T) {
 	tests := []struct {
 		word string
