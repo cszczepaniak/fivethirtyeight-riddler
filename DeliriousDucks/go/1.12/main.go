08f48a7fb4bcd7d6508ac43d13ef834ae950a4df
Connor Szczepaniak <cszczepaniak@gmail.com>
2020-01-17 11:33:20 -0600

    implement main

--- DeliriousDucks/go/1.12/main.go
+++ DeliriousDucks/go/1.12/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/cszczepaniak/fivethirtyeight-riddler/DeliriousDucks/game"
+)
+
+const (
+	width  = 3
+	height = 3
+)
+
+func main() {
+	g := game.New(game.Config{
+		BoardWidth:  3,
+		BoardHeight: 3,
+		NumDucks:    2,
+		StartX:      1,
+		StartY:      1,
+	})
+	i := g.Play()
+	fmt.Printf("ducks met after %d minutes\n", i)
 }
