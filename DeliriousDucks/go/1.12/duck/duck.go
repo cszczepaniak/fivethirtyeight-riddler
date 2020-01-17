d5db9ecf9909fba2faba15051081e9be9e2f0159
Connor Szczepaniak <cszczepaniak@gmail.com>
2020-01-17 10:54:56 -0600

    implement duck logic

---
+++ DeliriousDucks/go/1.12/duck/duck.go
@@ -0,0 +1,42 @@
+package duck
+
+import (
+	"math/rand"
+
+	"github.com/cszczepaniak/fivethirtyeight-riddler/DeliriousDucks/board"
+)
+
+type Duck struct {
+	Pos   board.Point
+	Board board.Board
+}
+
+func NewDuck(x, y int, b board.Board) Duck {
+	return Duck{
+		Pos:   board.NewPoint(x, y),
+		Board: b,
+	}
+}
+
+func (d *Duck) Move() {
+	pts := d.findNeighboringPoints()
+	d.Pos = pts[rand.Intn(len(pts))]
+}
+
+func (d *Duck) findNeighboringPoints() []board.Point {
+	// can have at most 4 neighboring points
+	pts := make([]board.Point, 0, 4)
+	for _, p := range d.Board {
+		if p.Y == d.Pos.Y {
+			if p.X == d.Pos.X+1 || p.X == d.Pos.X-1 {
+				pts = append(pts, p)
+			}
+		}
+		if p.X == d.Pos.X {
+			if p.Y == d.Pos.Y+1 || p.Y == d.Pos.Y-1 {
+				pts = append(pts, p)
+			}
+		}
+	}
+	return pts
+}
